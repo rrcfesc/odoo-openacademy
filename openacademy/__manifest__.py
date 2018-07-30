e4a5c9413119e2fc70bd4d225284691324737fd2
Ricardo Jesus Ruiz Cruz <rrcfesc@gmail.com>
2018-07-30 12:48:30 -0500

    Revert "[ADD]Terminating the module of secure"
    This reverts commit f789807b55ac961fcb5b21626f38006e66086ea8.

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -24,13 +24,11 @@
 
     # always loaded
     'data': [
-        'security/security.xml',
-        'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/partner.xml',
         'views/session.xml',
-
     ],
     # only loaded in demonstration mode
     'demo': [
